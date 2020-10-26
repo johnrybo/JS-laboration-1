45f8c746fdce43969b5b94626c1ccb1c67bb89cf
John Rybo <68517441+johnrybo@users.noreply.github.com>
2020-10-26 01:11:18 +0100

    skapat if else

---
+++ script.js
@@ -0,0 +1,33 @@
+function getAnswer() {
+
+    let gameContainer = document.getElementById('gameContainer');
+    let taskTitle = document.getElementById('taskTitle');
+    let taskDescription = document.getElementById('taskDescription');
+
+    var userInput = document.getElementById("answer").value;
+    
+    if (userInput === 'hoppa') {
+        taskDescription.innerText = 'Du valde att hoppa. För att komma vidare måste du...';
+    } else if (userInput === 'ducka') {
+        taskDescription.innerText = 'Du valde att ducka. För att komma vidare måste du...';
+    } else {
+        taskDescription.innerText = 'Du valde varken att hoppa eller att ducka. GAME OVER';
+     
+        // Hämtar "skicka svar-knappen" från DOM
+        let answerButton = document.getElementById('submitAnswer');
+
+        // Anger att answerButton inte ska visas
+        answerButton.style.display = 'none';
+
+        // Skapar en ny knapp
+        let startOverButton = document.createElement('button');
+
+        // Lägger til den nya knappen i gameContainer
+        gameContainer.append(startOverButton);
+
+        // Anger vad knappen ska innehålla för text
+        startOverButton.innerHTML = 'Börja om från början';
+
+    }
+
+}
