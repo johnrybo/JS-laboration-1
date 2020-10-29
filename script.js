bbd07aac6bce43b9c5914b0002c7597058321170
John Rybo <68517441+johnrybo@users.noreply.github.com>
2020-10-29 20:27:03 +0100

    diverse

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// gameContainer
+let gameContainer = document.getElementById('gameContainer');
+
 // taskDescription
 let taskDescription = document.getElementById("taskDescription");
 
+// Image container
+let imageContainer = document.getElementById('imageContainer');
+
 // Bilder
-let manWithHat = document.getElementById("manWithHat");
-let lion = document.getElementById('lion');
+let lion = document.createElement('img');
+    lion.src = 'images/lion.jpg';
+    lion.style.maxWidth = '300px';
+
+let manWithHat = document.createElement("img");
+    manWithHat.src = 'images/manWithHat.jpg';
+    manWithHat.style.maxWidth = '300px';
 
 // Input container
 let inputContainer = document.getElementById('inputContainer');
@@ -20,12 +31,21 @@ window.onload = startGame;
 
 function startGame() {
   taskDescription.innerText = scenes[0].description;
+  // loopScenes();
+  
 }
 
 function submitAnswer() {
   let userInput = document.getElementById("textInputField").value;
   task(userInput);
   textInputField.value = '';
+
+  addImage(manWithHat, 1);
+  removeImage(manWithHat, 1);
+
+  addImage(lion, 2);
+  removeImage(lion, 2);
+  console.log(userInput);
 }
 
 function startOver() {
@@ -40,6 +60,7 @@ function task(input) {
 
       currentScene = scenes[currentScene].nextScene[0];
       taskDescription.innerText = scenes[currentScene].description;
+      taskImage = scenes[currentScene]
 
       } else if (input == scenes[currentScene].options[1]) {
 
@@ -58,6 +79,19 @@ function task(input) {
           }
 }
 
+
+function addImage(image, scene){    
+    if (taskDescription.innerText == scenes[scene].description) {
+      imageContainer.append(image);
+    }
+}
+    
+function removeImage(image, scene) {
+    if (taskDescription.innerText !== scenes[scene].description) {
+      image.style.display = 'none';
+  }
+}
+
 let scenes = [
   {
     id: 0,
@@ -67,13 +101,13 @@ let scenes = [
   },
   {
     id: 1,
-    description: 'Vill du springa eller flyga? (id: 1)',
+    description: 'En man med röd hatt undrar om du vill du springa eller flyga? (id: 1)',
     options: ['springa', 'flyga'],
     nextScene: [2, 2]
   },
   {
     id: 2,
-    description: 'Vill du sjunga eller prata (id: 2)',
+    description: 'Ett lejon undrar om du vill du sjunga eller prata (id: 2)',
     options: ['sjunga', 'prata'],
     nextScene: [3, 4]
   },
@@ -96,159 +130,3 @@ let scenes = [
     nextScene: [0]
   }
 ];
-
-//////////////////////////////////////////////////////////////////////////////////
-
-// Funktioner för knapparna
-
-/*
-function submitAnswer1() {
-  let userInput = document.getElementById("textInputField").value;
-  task1(userInput);
-  textInputField.value = '';
-}
-
-function submitAnswer2_1() {
-  let userInput = document.getElementById("textInputField").value;
-  task2_hoppa(userInput);
-  textInputField.value = '';
-}
-
-function submitAnswer2_2() {
-  let userInput = document.getElementById("textInputField").value;
-  task2_ducka(userInput);
-  textInputField.value = '';
-}
-
-function submitAnswer3_1() {
-    let userInput = document.getElementById("textInputField").value;
-    task3_1(userInput);
-    textInputField.value = '';
-  }
-
-  function submitAnswer3_2() {
-    let userInput = document.getElementById("textInputField").value;
-    task3_2(userInput);
-    textInputField.value = '';
-  }
-  */
-
-// Funktioner för olika tasks
-
-/*
-function task1(input) {
-
-  if (input == "hoppa") {
-    taskTitle.innerText = "Uppgift 2.1";
-    taskDescription.innerText =
-      "Snyggt hoppat! Du blev väldigt törstig av hoppandet och bestämmer dig för att gå och köpa en öl, men för att komma in på den närmsta baren ber dörrvakten dig att svara rätt på följande kluriga mattetal: vad är 5 + 5?";
-
-    answer1Button.style.display = "none";
-    answer2_1Button.style.display = "block";
-
-  } else if (input == "ducka") {
-    taskTitle.innerText = "Uppgift 2.2";
-    taskDescription.innerText =
-      "En man som blev imponerad av hur snabbt du duckade kommer fram till dig och undrar vilken färg hans hatt är. Hjälp honom för att gå vidare";
-
-    manWithHat.style.display = "block";
-    answer1Button.style.display = "none";
-    answer2_2Button.style.display = "block";
-
-  } else {
-    taskDescription.innerText =
-      "Du valde varken att hoppa eller att ducka och blev träffad av objektet. GAME OVER!";
-
-    answer1Button.style.display = "none";
-    textInputField.style.display = 'none';
-    startOverButton.style.display = "block";
-  }
-}
-
-function task2_hoppa(input) {
-    
-    if (input == '10') {
-        taskTitle.innerText = "Uppgift 3.1";
-        taskDescription.innerText =
-            "Korrekt! Du har precis hunnit köpa din öl och sätta dig på uteserveringen när du hör ett högt läskigt ljud från andra sidan gatan. Du vänder dig om och ser den här varelsen, men vad är det för djur? ";
-
-            lion.style.display = "block";
-            answer2_1Button.style.display = "none";
-            answer3_1Button.style.display = "block";
-
-    } else if (input !== '10') {
-        taskDescription.innerText =
-            "Fel! Rätt svar är: 10. Dörrvakten släppte inte in dig. GAME OVER!";
-
-        answer2_1Button.style.display = "none";
-        textInputField.style.display = 'none';
-        startOverButton.style.display = "block";
-    }
-}
-
-function task2_ducka(input) {
-    
-    if (input == 'röd') {
-        taskTitle.innerText = "Uppgift 3.2";
-        taskDescription.innerText =
-            "Korrekt! Mannen med den röda hatten visade sig vara webbutvecklare men har glömt bort vad Googles webbläsare heter, minns du detta tro?";
-
-            manWithHat.style.display = "none";
-            answer2_2Button.style.display = "none";
-            answer3_2Button.style.display = "block";
-
-    } else if (input !== 'röd') {
-        taskDescription.innerText =
-            "Fel! Rätt svar är: röd. En person som paserade på gatan hörde att du svarade fel och skällde ut dig. GAME OVER!";
-
-        manWithHat.style.display = "none";
-        answer2_2Button.style.display = "none";
-        textInputField.style.display = 'none';
-        startOverButton.style.display = "block";
-    }
-}
-
-function task3_1(input) {
-
-    if (input == 'lejon') {
-        taskTitle.innerText = "Bra jobbat!";
-        taskDescription.innerText =
-            "Korrekt! Du har klarat samtliga uppgifter! Klicka på knappen nedan för att börja om från början";
-
-        lion.style.display = "none";
-        answer3_1Button.style.display = "none";
-        textInputField.style.display = 'none';
-        startOverButton.style.display = "block";
-
-    } else if (input !== 'lejon') {
-    taskDescription.innerText =
-        "Fel! Rätt svar är: lejon. Du blev tyvärr uppäten. GAME OVER!";
-
-    lion.style.display = "none";
-    answer3_1Button.style.display = "none";
-    textInputField.style.display = 'none';
-    startOverButton.style.display = "block";
-}
-}
-
-function task3_2(input) {
-    
-    if (input == 'chrome') {
-        taskTitle.innerText = "Bra jobbat!";
-        taskDescription.innerText =
-            "Korrekt! Du har klarat samtliga uppgifter! Klicka på knappen nedan för att börja om från början";
-
-        answer3_2Button.style.display = "none";
-        textInputField.style.display = 'none';
-        startOverButton.style.display = "block";
-
-    } else if (input !== 'chrome') {
-    taskDescription.innerText =
-        "Fel! Rätt svar är: chrome. GAME OVER!";
-
-    answer3_2Button.style.display = "none";
-    textInputField.style.display = 'none';
-    startOverButton.style.display = "block";
-}
-}
-*/
