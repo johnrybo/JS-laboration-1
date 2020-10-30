7cab30b7ab6a775a3b193dc776b87ba2f0cd2297
John Rybo <68517441+johnrybo@users.noreply.github.com>
2020-10-30 11:10:45 +0100

    diverse

--- script.js
+++ script.js
@@ -92,70 +92,60 @@ function imageFunction() {
 
 let scenes = [
   {
-    id: 0,
     description: "Du går runt i en ny spännande stad när ett farligt objekt plötsligt kommer flygande emot dig. Vill du hoppa eller ducka för att undvika att bli träffad?",
     options: ['ducka', 'hoppa'],
     // image: undefined,
     nextScene: [1, 2]
   },
   {
-    id: 1,
     description: 'En man som blev imponerad av hur snabbt du duckade undrar om du kan berätta för honom vilken färg hans hatt är.',
     options: ['röd'],
     image: manWithHat,
     nextScene: [3]
   },
   {
-    id: 2,
     description: 'Efter att du landat från ditt hopp ser du ett lejon som står och vrålar runt hörnet. Vill du gå fram och klappa? (ja / nej) ',
     options: ['ja', 'nej'],
     image: lion,
     nextScene: [4, 5]
   },
   {
-    id: 3,
     description: 'Mannen med den röda hatten blev otroligt tacksam och gav dig 50 kr i belöning. Vill du spara pengarna eller gå och köpa öl? (spara / köpa öl)',
     options: ['spara', 'köpa öl'],
     // image: undefined,
     nextScene: [6, 7]
   },
   {
-    id: 4,
     description: 'Du blev tyvärr uppäten av lejonet. Skriva svaret på 5 + 5 för att börja om från början med hedern i behåll.',
     options: ['10'],
     // image: undefined,
     nextScene: [0]
   },
   {
-    id: 5,
     description: 'Smart val att inte gå fram och klappa lejonet. Som om det inte vore nog hittar du 50 kr på marken framför dig, inte helt omöjligt att en man med röd hatt kan ha tappat den. Hursomhelst, vill du spara pengarna eller gå och köpa öl? (spara / köpa öl)',
     options: ['spara', 'köpa öl'],
     // image: undefined,
     nextScene: [6, 7]
   },
   {
-    id: 6,
     description: 'Duktigt och ekonomiskt av dig. Vill du gå in på närmsta bank och sätta in pengarna på ditt konto istället? (ja / nej)',
     options: ['ja', 'nej'],
     image: bank,
     nextScene: [8, 9]
   },
   {
-    id: 7,
     description: 'Mmm vad gott det var med en öl. Du har fattat många bra beslutet under spelets gång! Skriv "jag är smart" för att börja om från början.',
     options: ['jag är smart'],
     image: beer,
     nextScene: [0]
   },
   {
-    id: 8,
     description: 'Pengarna är inne på kontot inom 1-2 bankdagar. Slutet gott allting gott. Skriv "om jag fortsätter såhär kommer jag snart bli rik" för att börja om från början.',
     options: ['om jag fortsätter såhär kommer jag snart bli rik'],
     // image: undefined,
     nextScene: [0]
   },
   {
-    id: 9,
     description: 'Dumt val! En rånare dök upp precis efter att du gått förbi banken och du blev av med dina pengar. Skriv "skärpning" för att börja om från början.',
     options: ['skärpning'],
     image: robbery,
