e4a092d84627bae2ac106f387c0033761a0becbd
John Rybo <68517441+johnrybo@users.noreply.github.com>
2020-10-30 11:09:31 +0100

    diverse

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let textInputField = document.getElementById('textInputField');
 let sendAnswerbutton = document.getElementById('sendAnswerButton');
     sendAnswerbutton.addEventListener("click", submitAnswer);
 
+// Bilder
 let lion = document.createElement('img');
     lion.src = 'images/lion.jpg';
     lion.style.maxWidth = '15rem';
@@ -13,47 +14,64 @@ let manWithHat = document.createElement("img");
     manWithHat.src = 'images/manWithHat.jpg';
     manWithHat.style.maxWidth = '15rem';
 
+let beer = document.createElement("img");
+    beer.src = 'images/beer.jpg';
+    beer.style.maxWidth = '15rem';
+
+let bank = document.createElement("img");
+    bank.src = 'images/bank.jpg';
+    bank.style.maxWidth = '15rem';
+
+let robbery = document.createElement("img");
+    robbery.src = 'images/robbery.jpg';
+    robbery.style.maxWidth = '15rem';
+
 window.onload = startGame;
 
+/** Startar spelet */
 function startGame() {
   taskDescription.innerText = scenes[0].description;
 }
 
+/** Bestämmer vad som händer när användaren svarat på en fråga */
 function submitAnswer() {
   let userInput = document.getElementById("textInputField").value;
   task(userInput);
   textInputField.value = '';
 
-  addImage(manWithHat, 1);
-  removeImage(manWithHat, 1);
-
-  addImage(lion, 2);
-  removeImage(lion, 2);
+  imageFunction();
 }
 
+/** Startar om spelet genom att ladda om sidan */
 function startOver() {
   location.reload();
 }
 
 let currentScene = 0;
 
+/**
+ * Flyttar användaren mellan olika scener beroende på vad användaren svarar
+ * @param {*} input Användarens svar i form av text som anges i textfältet
+ */
 function task(input) {
 
     if (input == scenes[currentScene].options[0]) {
 
       currentScene = scenes[currentScene].nextScene[0];
       taskDescription.innerText = scenes[currentScene].description;
-      taskImage = scenes[currentScene]
 
       } else if (input == scenes[currentScene].options[1]) {
 
         currentScene = scenes[currentScene].nextScene[1];
         taskDescription.innerText = scenes[currentScene].description;
+        imageContainer.append(scenes[currentScene].image);
 
         } else {
           taskDescription.innerText = 'Ditt svar var ej giltigt. GAME OVER!';
           sendAnswerButton.style.display = 'none';
           textInputField.style.display = 'none';
+          imageContainer.style.display = 'none';
+
           let restartGameButton = document.createElement('button');
           restartGameButton.innerText = 'Börja om från början';
           restartGameButton.classList.add('redButton');
@@ -62,78 +80,85 @@ function task(input) {
           }
 }
 
-function addImage(image, scene){    
-    if (taskDescription.innerText == scenes[scene].description) {
-      imageContainer.append(image);
-      image.style.display = 'block';
-    }
-}
-    
-function removeImage(image, scene) {
-    if (taskDescription.innerText !== scenes[scene].description) {
-      image.style.display = 'none';
-  }
+/** Lägger till en bild till den aktuella scenen om det finns ett image-attribut som inte är undefined */
+function imageFunction() {
+  if (scenes[currentScene].image === undefined) {
+    imageContainer.innerHTML = "";
+  } else {
+    imageContainer.innerHTML = "";
+    imageContainer.append(scenes[currentScene].image);
+  } 
 }
 
 let scenes = [
   {
     id: 0,
-    description: "Du går runt i en ny spännande stad när ett farligt objekt plötsligt kommer flygande emot dig, vill du hoppa eller ducka för att undvika att bli träffad? DUCKA / HOPPA (index: 0)",
+    description: "Du går runt i en ny spännande stad när ett farligt objekt plötsligt kommer flygande emot dig. Vill du hoppa eller ducka för att undvika att bli träffad?",
     options: ['ducka', 'hoppa'],
+    // image: undefined,
     nextScene: [1, 2]
   },
   {
     id: 1,
-    description: 'En man som blev imponerad av hur snabbt du duckade undrar om du kan berätta för honom vilken färg hans hatt är. RÖD (index: 1)',
+    description: 'En man som blev imponerad av hur snabbt du duckade undrar om du kan berätta för honom vilken färg hans hatt är.',
     options: ['röd'],
+    image: manWithHat,
     nextScene: [3]
   },
   {
     id: 2,
-    description: 'Efter att du landat från ditt hopp ser du ett lejon som står och vrålar runt hörnet. Vill du gå fram och klappa? JA / NEJ (index: 2)',
+    description: 'Efter att du landat från ditt hopp ser du ett lejon som står och vrålar runt hörnet. Vill du gå fram och klappa? (ja / nej) ',
     options: ['ja', 'nej'],
+    image: lion,
     nextScene: [4, 5]
   },
   {
     id: 3,
-    description: 'Mannen med den röda hatten blev otroligt tacksam och gav dig 50 kr i belöning. Vill du spara pengarna eller gå och köpa en öl? SPARA / KÖPA ÖL (index: 3)',
+    description: 'Mannen med den röda hatten blev otroligt tacksam och gav dig 50 kr i belöning. Vill du spara pengarna eller gå och köpa öl? (spara / köpa öl)',
     options: ['spara', 'köpa öl'],
+    // image: undefined,
     nextScene: [6, 7]
   },
   {
     id: 4,
-    description: 'Du blev tyvärr uppäten av lejonet. Skriva svaret på 5 + 5 för att börja om från början med hedern i behåll. 10 (index: 4)',
+    description: 'Du blev tyvärr uppäten av lejonet. Skriva svaret på 5 + 5 för att börja om från början med hedern i behåll.',
     options: ['10'],
+    // image: undefined,
     nextScene: [0]
   },
   {
     id: 5,
-    description: 'Smart val att inte gå fram och klappa lejonet. Som om det inte vore nog hittar du 50 kr på marken framför dig, inte helt omöjligt att en man med röd hatt kan ha tappat den. Hursomhelst, vill du spara pengarna eller gå och köpa öl? SPARA / KÖPA ÖL (index: 5)',
+    description: 'Smart val att inte gå fram och klappa lejonet. Som om det inte vore nog hittar du 50 kr på marken framför dig, inte helt omöjligt att en man med röd hatt kan ha tappat den. Hursomhelst, vill du spara pengarna eller gå och köpa öl? (spara / köpa öl)',
     options: ['spara', 'köpa öl'],
+    // image: undefined,
     nextScene: [6, 7]
   },
   {
     id: 6,
-    description: 'Duktigt och ekonomiskt av dig. Vill du gå in på närmsta bank och sätta in pengarna på ditt konto istället? JA / NEJ (index: 6)',
+    description: 'Duktigt och ekonomiskt av dig. Vill du gå in på närmsta bank och sätta in pengarna på ditt konto istället? (ja / nej)',
     options: ['ja', 'nej'],
+    image: bank,
     nextScene: [8, 9]
   },
   {
     id: 7,
-    description: 'Mmm vad gott det var med en öl. Du har fattat många bra beslutet under spelets gång! Skriv "ja" för att börja om från början. JA (index: 7)',
-    options: ['ja'],
+    description: 'Mmm vad gott det var med en öl. Du har fattat många bra beslutet under spelets gång! Skriv "jag är smart" för att börja om från början.',
+    options: ['jag är smart'],
+    image: beer,
     nextScene: [0]
   },
   {
     id: 8,
-    description: 'Pengarna är inne på kontot inom 1-2 bankdagar. Slutet gott allting gott. Skriv "ja" för att börja om från början. JA (index: 8)',
-    options: ['ja'],
+    description: 'Pengarna är inne på kontot inom 1-2 bankdagar. Slutet gott allting gott. Skriv "om jag fortsätter såhär kommer jag snart bli rik" för att börja om från början.',
+    options: ['om jag fortsätter såhär kommer jag snart bli rik'],
+    // image: undefined,
     nextScene: [0]
   },
   {
     id: 9,
-    description: 'Dumt val! En rånare dök upp precis efter att du gått förbi banken och du blev av med dina pengar. Skriv "ja" för att börja om från början. JA (index: 9)',
-    options: ['ja'],
+    description: 'Dumt val! En rånare dök upp precis efter att du gått förbi banken och du blev av med dina pengar. Skriv "skärpning" för att börja om från början.',
+    options: ['skärpning'],
+    image: robbery,
     nextScene: [0]
   }
 ];
